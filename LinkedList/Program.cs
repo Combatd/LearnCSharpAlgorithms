94b3000fa60c7d4487459fbc6a35777cb6dfc64f
Combatd <combatdstar@gmail.com>
2021-03-07 16:29:27 -0800

    Declare SinglyLinkedList, Node classes

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -10,8 +10,51 @@ namespace SinglyLinkedList
     {
         static void Main(string[] args)
         {
-            //TODO
+            // TODO
         }
     }
+
+    public class SinglyLinkedList
+    {
+        private Node first; // the head
+        // if the first node is null, we know we have no nodes
+        public bool isEmpty()
+        {
+            return (first == null);
+        }
+
+        // we need to be able to insert the node at the front of the LinkedList
+        public void insertFirst(int data)
+        {
+            Node newNode = new Node();
+            newNode.data = data; // takes value of param data
+            newNode.next = first; // points to the first node
+            first = newNode;
+        }
+
+        // delete the first Node, the head, in the LinkedList
+        public Node deleteFirst()
+        {
+            Node temp = first;
+            first = first.next;
+            return temp; // returns the removed Node, formerly the first
+        }
+
+
+
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next; // should be able to show the next node
+
+        public void displayNode()
+        {
+            Console.WriteLine($"<  {data}  >");
+        }
+    }
+
+
 
 }

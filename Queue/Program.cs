23dd07722cff299e1bad58f3e14d6cfe43251841
Combatd <combatdstar@gmail.com>
2021-03-10 10:08:12 -0800

    Define Queue#insert, #isFull boolean method not implemented

--- Queue/Program.cs
+++ Queue/Program.cs
@@ -27,6 +27,20 @@ namespace QueueDemo
             items = 0; // track number of pointers
         }
 
+        public void insert(long j)
+        {
+            //if (isFull())
+            //{
+            //    Console.WriteLine("The queue is full.");
+            //}
 
+            if (rear == maxSize - 1)
+            {
+                rear = -1; // assign the -1 to rear
+            }
+            rear++;
+            myQueue[rear] = j;
+            items++;
+        }
     }
 }

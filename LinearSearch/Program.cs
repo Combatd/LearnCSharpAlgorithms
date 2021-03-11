4de3d487556de554013bd990b2fdbde14ad899c2
Combatd <combatdstar@gmail.com>
2021-03-11 10:29:49 -0800

    Define LinearSearch#linearSearch

--- LinearSearch/Program.cs
+++ LinearSearch/Program.cs
@@ -8,5 +8,27 @@ namespace LinearSearch
         {
             //TODO
         }
+
+        /// <summary>
+		/// a = array of integers
+		/// x = value to search for
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="x"></param>
+		/// <returns></returns>
+
+        static int linearSearch(int [] a, int x)
+        {
+            for (int = 0; int < a.Length; int++)
+            {
+                if (a[i] == x)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
     }
 }
